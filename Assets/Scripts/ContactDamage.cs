8df503bb2620fe913aeba1d2cc44e7a1cdc31bda
OlekHla <sashko.hladesh@gmail.com>
2026-04-24 13:53:55 +0200

    minor changes + spikes kill

--- Assets/Scripts/ContactDamage.cs
+++ Assets/Scripts/ContactDamage.cs
@@ -1,16 +1,35 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageOnHit : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    new private Collider2D collider; //The "new" keyword is there to suppress the icky warning over this variable name hiding an already deprecated property
+    public float damage = 1;
+    [SerializeField] private float damageCooldown = 0.25f;
+    private float timer = 0f;
+
+    void Awake()
     {
-        
+        collider = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    protected void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > damageCooldown)
+        {
+            timer = 0;
+            List<Collider2D> res = new List<Collider2D>();
+            collider.Overlap(res);
+            foreach (Collider2D col in res)
+            { //Iterating over overlapping colliders
+                GameObject targetOwner = col.gameObject; //Getting the owner of the collider (in case we have an enemy with several colliders or smth)
+                if (targetOwner.tag != "Entity" && targetOwner.tag != "Player") { continue; }
+
+                RobotSamurai victim = targetOwner.GetComponent<RobotSamurai>();
+                victim.TakeDamage(damage);
+            }
+        }
     }
 }
