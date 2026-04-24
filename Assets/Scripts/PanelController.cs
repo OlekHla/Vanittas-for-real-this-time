f0d4106867546a9fe32122ca2f98bd9540ad215c
OlekHla <sashko.hladesh@gmail.com>
2026-04-24 17:58:54 +0200

    sghioqwigoqg

--- Assets/Scripts/PanelController.cs
+++ Assets/Scripts/PanelController.cs
@@ -25,7 +25,7 @@ public class PanelController : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void ExitGame()
