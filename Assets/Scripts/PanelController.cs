1c5d68dea3ce9a3c58ab7f399d4d0987123ed891
kalinaszklarek-art <kalina.szklarek@gmail.com>
2026-04-24 12:25:31 +0200

    main menu dodane jako nowa scena

---
+++ Assets/Scripts/PanelController.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static UnityEngine.Rendering.DebugUI;
+
+public class PanelController : MonoBehaviour
+{
+    public List<Panel> panels = new List<Panel>();
+
+    public void ShowPanel(Panel panelToShow)
+    {
+        foreach (var panel in panels)
+        {
+            if (panelToShow == panel)
+            {
+                panel.gameObject.SetActive(true);
+            }
+            else
+            {
+                panel.gameObject.SetActive(false);
+            }
+
+            //panel.gameObject.SetActive(panelToShow == panel); <-- to samo co ca³y if else
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
